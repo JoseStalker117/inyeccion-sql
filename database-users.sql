1811f8bf973887eca666d755fd9c2017d608cc26
JoseStalker117 <josefriki23@gmail.com>
2025-07-13 19:58:01 -0600

    Correccion de sintaxis de mysql a express

--- database-users.sql
+++ database-users.sql
@@ -1,22 +1,30 @@
-CREATE DATABASE IF NOT EXISTS USUARIOS;
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'USUARIOS')
+BEGIN
+    CREATE DATABASE USUARIOS;
+END
 GO
 
 USE USUARIOS;
 GO
 
-
-CREATE TABLE IF NOT EXISTS USUARIOS (
-    id INT PRIMARY KEY IDENTITY(1,1),
-    username NVARCHAR(50) NOT NULL,
-    password NVARCHAR(50) NOT NULL
-);
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='USUARIOS' AND xtype='U')
+BEGIN
+    CREATE TABLE USUARIOS (
+        id INT PRIMARY KEY IDENTITY(1,1),
+        username NVARCHAR(50) NOT NULL,
+        password NVARCHAR(50) NOT NULL
+    );
+END
 GO
 
-INSERT INTO USUARIOS (username, password) VALUES
-('jmaiv117','admin123'),
-('anahis68','superduper'),
-('josefriki23','sindromeimpostor'),
-('landaverde','mainrakan'),
-('izumi','mainsamira'),
-('annotheralgo','mainxayah');
+IF NOT EXISTS (SELECT TOP 1 1 FROM USUARIOS)
+BEGIN
+    INSERT INTO USUARIOS (username, password) VALUES
+    ('jmaiv117','admin123'),
+    ('anahis68','superduper'),
+    ('josefriki23','sindromeimpostor'),
+    ('landaverde','mainrakan'),
+    ('izumi','mainsamira'),
+    ('annotheralgo','mainxayah');
+END
 GO
